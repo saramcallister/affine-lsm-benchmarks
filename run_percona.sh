3e74416d65a00849ba0883291c742f1bc849c05d
saramcallister <smcallister@g.hmc.edu>
2018-12-28 09:53:29 -0800

    Changed path

--- run_percona.sh
+++ run_percona.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 file="/mnt/db/affine_benchmark_data"
 output="betree.out"
+build_path="PerconaFT/build/ft/tests"
 
 for keysize in 1024
 do
@@ -11,14 +12,12 @@ do
 	echo
 	echo
 
-	#for block_size in "0.25" "0.5" 1 2 4 8 16 32 64 
-	for block_size in 128
-	#for block_size in "0.125" "0.0625"
+	for block_size in "0.25" "0.5" 1 2 4 8 16 32 64 128
 	do
 		[[ -e $file ]] && rm $file
 
-		./affine_bench_prep_data $block_size $keysize $valuesize | tee -a $output
-		./affine_bench_warm_cache $block_size $keysize $valuesize | tee -a $output
+		../$build_path/affine_bench_prep_data $block_size $keysize $valuesize | tee -a $output
+		../$build_path/affine_bench_warm_cache $block_size $keysize $valuesize | tee -a $output
 		
 		#log="iostat_${block_size}.out"
 		#date >> $log
@@ -26,11 +25,11 @@ do
 		#iostat -t -d /dev/sda3 1 180 >> $log &
 		#sleep 10
 		#echo "Query start" >> $log
-		./affine_bench_query $block_size $keysize $valuesize | tee -a $output
+		../$build_path/affine_bench_query $block_size $keysize $valuesize | tee -a $output
 		#echo "Insert start" >> $log 
-		./affine_bench_insert $block_size $keysize $valuesize | tee -a $output
+		../$build_path/affine_bench_insert $block_size $keysize $valuesize | tee -a $output
 		#echo "Delete start" >> $log
-		./affine_bench_delete $block_size $keysize $valuesize | tee -a $output
+		../$build_path/affine_bench_delete $block_size $keysize $valuesize | tee -a $output
 		#date >> $log
 		#sleep 10
 	done
